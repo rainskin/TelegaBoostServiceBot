5ae9bee4ecc22550304eb5720790fdb2ecb2c111
rainskin <roma.berlev97@yandex.ru>
2025-06-23 23:49:25 +0500

    Refactor commission retrieval in convert_to_stars function

--- src/utils/currencies/telegram_stars.py
+++ src/utils/currencies/telegram_stars.py
@@ -3,13 +3,6 @@ import math
 from core.db import admin
 from utils.currencies import usdt
 
-commissions = admin.get_recharge_xtr_commissions()
-TELEGRAM_STAR_TO_USD_EQUIVALENT = commissions.get('telegram_star_to_usd_equivalent')
-TON_TO_USDT_SPOT_FEE_PERCENT = commissions.get('ton_to_usdt_spot_fee_percent')
-TON_NETWORK_FEE_PERCENT = commissions.get('ton_network_fee_percent')
-
-P2P_PREMIUM_PERCENT = commissions.get('p2p_premium_percent_from_db')
-
 
 def convert_to_stars(service_price_rub):
     """
@@ -22,6 +15,14 @@ def convert_to_stars(service_price_rub):
     Returns:
         Int: The number of Telegram Stars, rounded up, or None in case of error.
     """
+
+    commissions = admin.get_recharge_xtr_commissions()
+    telegram_star_to_usd_equivalent = commissions.get('telegram_star_to_usd_equivalent')
+    ton_to_usdt_spot_fee_percent = commissions.get('ton_to_usdt_spot_fee_percent')
+    ton_network_fee_percent = commissions.get('ton_network_fee_percent')
+
+    p2p_premium_percent = commissions.get('p2p_premium_percent_from_db')
+
     if service_price_rub <= 0:
         print("Error: The price of the service in rubles should be positive.")
         return None
@@ -33,14 +34,14 @@ def convert_to_stars(service_price_rub):
         return None
 
     # 2. Рассчитываем P2P_USDT_TO_RUB_RATE
-    if P2P_PREMIUM_PERCENT < 0:
+    if p2p_premium_percent < 0:
         print("Error: The percentage of the P2P invoice cannot be negative.")
         return None
 
-    p2p_usdt_to_rub_rate = usd_to_rub_rate * (1 + P2P_PREMIUM_PERCENT)
+    p2p_usdt_to_rub_rate = usd_to_rub_rate * (1 + p2p_premium_percent)
     print(
         f"Рассчитанный P2P USDT/RUB курс "
-        f"(с наценкой {P2P_PREMIUM_PERCENT * 100:.2f}%): 1 USDT ≈ {p2p_usdt_to_rub_rate:.4f} RUB")
+        f"(с наценкой {p2p_premium_percent * 100:.2f}%): 1 USDT ≈ {p2p_usdt_to_rub_rate:.4f} RUB")
 
     # 3. How many USDT need to get clean on p2p
     try:
@@ -50,20 +51,20 @@ def convert_to_stars(service_price_rub):
         return None
 
     # 4. Сколько USD нужно, чтобы хватило после всех комиссий (спот + газ TON)
-    if TON_TO_USDT_SPOT_FEE_PERCENT >= 1.0 or TON_TO_USDT_SPOT_FEE_PERCENT < 0:
+    if ton_to_usdt_spot_fee_percent >= 1.0 or ton_to_usdt_spot_fee_percent < 0:
         print("Ошибка: Процент спотовой комиссии должен быть между 0 и 1.")
         return None
 
-    usd_after_ton_spot_fee = required_usdt_net / (1 - TON_TO_USDT_SPOT_FEE_PERCENT)
+    usd_after_ton_spot_fee = required_usdt_net / (1 - ton_to_usdt_spot_fee_percent)
 
-    if TON_NETWORK_FEE_PERCENT >= 1.0 or TON_NETWORK_FEE_PERCENT < 0:
+    if ton_network_fee_percent >= 1.0 or ton_network_fee_percent < 0:
         print("Ошибка: Процент сетевой комиссии TON должен быть между 0 и 1.")
         return None
 
-    usd_total_required = usd_after_ton_spot_fee / (1 - TON_NETWORK_FEE_PERCENT)
+    usd_total_required = usd_after_ton_spot_fee / (1 - ton_network_fee_percent)
 
     # 5. Сколько Stars требуется
-    num_stars = math.ceil(usd_total_required / TELEGRAM_STAR_TO_USD_EQUIVALENT)
+    num_stars = math.ceil(usd_total_required / telegram_star_to_usd_equivalent)
 
     # print(f"\n--- Детализация расчета ---")
     # print(f"Исходная цена услуги (RUB): {service_price_rub:.2f} RUB")
